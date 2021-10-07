ac5b149212490f5d6100672ae93dcd55715f0cd6
DiegoMPMS <4868779@gmail.com>
2021-10-07 18:23:14 -0300

    Função de propaganda adicionada, novo padrão para tratamento de commandos

--- bot.js
+++ bot.js
@@ -4,6 +4,7 @@ const dotenv = require('dotenv').config();
 
 if (dotenv.error) { throw result.error }
 
+//opções do bot, login, token, canais para acessar, debug, ssl, reconnect
 const opts = {
     //identity é a conta que bot usa e autorização
     options: { debug: false },
@@ -20,6 +21,9 @@ const opts = {
         'lieslieslies',
     ] 
 };
+//Opções de Propaganda
+const intervalo = parseInt(process.env.INTERVALO_PROPAGANDA) * 60000;
+const duracao = parseInt(process.env.DURACAO_PROPAGANDA);
 
 //criar o client que usando as credenciais forncedas se conecta aos canais informados
 const client = new tmi.client(opts);
@@ -39,21 +43,65 @@ client.on('connected', onConnectedHandler);
 //tentar conexão
 client.connect();
 
+//============  Área para Timed Events ==================
+var prop_interval = setInterval(propaganda, intervalo, opts.channels[1], duracao);
+;
+
 //handler de msgs
 function onMessageHandler(channel, tags, msg, self){
     //ignora msgs vindas do próprio bot.
-    if (self) {return;};
-    //printa no console o nome da pessoa e a msg enviada por ela
+    if (self || !msg.startsWith('!')) {return;};
+    
+    //slice remove o primeiro elemento aka !
+    //split gera um array de strings apartir do input msg, dividindo quando vê ' '
     msg = msg.trim();
-    msg = msg.toLowerCase();
-    console.log(msg);
-    if (msg === '!olabot') {
-        client.say(channel, `@${tags.username}, Hello There.`)
+    args = msg.slice(1).split(' ');
+    //shift remove o primeiro elemento do array nesse caso ele remove o comando e coloca em command,
+    //e os demais argumentos continuam em args.
+    command = args.shift().toLowerCase();
+    
+    console.log(command);
+    //Roll Dices
+    if (command === 'dice') {
+        dice(args.shift().toLowerCase(), args.shift().toLowerCase(), channel, tags);
+    } else if (command === 'placeholder'){
+        return
     }
-
+    
 };
 
 //handler do evento de conexão
 function onConnectedHandler (addr, port) {
     console.log(`* Connected to ${addr}:${port}`);
 };
+
+//função para rolagem de dados
+function dice(lados, quantidade, channel, tags){
+    try {
+        lados = parseInt(lados);
+        quantidade = parseInt(quantidade);
+        if (quantidade > 20 || lados > 100) {
+            client.say(channel, `@${tags.username}, щ（ﾟДﾟщ） eu só consigo rolar no máximo 20 dados de até 100 lados.`);
+            return;
+        }
+        var resultado = [];
+        for (let i = 0; i < quantidade; i++){
+            resultado.push(Math.floor(Math.random() * lados) + 1);
+        }
+        client.say(channel, `@${tags.username}, você rolou: ${resultado.join(' - ')}`)
+    } catch (e){
+        client.say(channel, `@${tags.username}, : Erro nos argumentos. < !dice <lados> <quantidade> > Ex: "!dice 20 3", roda 3 d20.`)
+    }
+}
+
+//função de propaganda
+function propaganda(channel, duracao){
+    client.commercial(channel, duracao)
+.then((data) => {
+    // data returns [channel, seconds]
+    console.log(data);
+}).catch((err) => {
+    //promisse rejection catch
+    console.log("error: ",err);
+});
+}
